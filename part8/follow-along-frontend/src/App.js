8706bbd2786171b13504eab4f01bfee31372e29d
Tzvi-H <16guitar@gmail.com>
2022-08-08 16:32:26 -0600

    completed 8b

--- part8/follow-along-frontend/src/App.js
+++ part8/follow-along-frontend/src/App.js
@@ -1,24 +1,40 @@
-import { gql, useQuery } from "@apollo/client";
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
 import Persons from "./components/Persons";
-
-const ALL_PERSONS = gql`
-  query {
-    allPersons {
-      name
-      phone
-      id
-    }
-  }
-`;
+import PersonForm from "./components/PersonForm";
+import PhoneForm from "./components/PhoneForm";
+import { ALL_PERSONS } from "./queries";
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
   const result = useQuery(ALL_PERSONS);
 
   if (result.loading) {
     return <div>loading...</div>;
   }
 
-  return <Persons persons={result.data.allPersons} />;
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
+
+  return (
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
+    </div>
+  );
+};
+
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null;
+  }
+  return <div style={{ color: "red" }}>{errorMessage}</div>;
 };
 
 export default App;
