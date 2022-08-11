ca3952f1354a28fbd00633bd5111ba10496f66ba
Tzvi-H <16guitar@gmail.com>
2022-08-11 15:11:38 -0600

    completed 8.10

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -21,3 +21,25 @@ export const ALL_BOOKS = gql`
     }
   }
 `;
+
+export const ADD_BOOK = gql`
+  mutation AddBook(
+    $title: String!
+    $author: String!
+    $published: Int!
+    $genres: [String]
+  ) {
+    addBook(
+      title: $title
+      author: $author
+      published: $published
+      genres: $genres
+    ) {
+      title
+      published
+      author
+      genres
+      id
+    }
+  }
+`;
