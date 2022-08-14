54bf62f3359aafc7d87d9861368dc2d2a5fbc4a6
Tzvi-H <16guitar@gmail.com>
2022-08-14 15:33:42 -0600

    completed 8.17

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
-const { ApolloServer, gql, UserInputError } = require("apollo-server");
+const {
+  ApolloServer,
+  gql,
+  UserInputError,
+  AuthenticationError,
+} = require("apollo-server");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const Author = require("./models/author");
@@ -93,7 +98,11 @@ const resolvers = {
   },
 
   Mutation: {
-    addBook: async (root, args) => {
+    addBook: async (root, args, context) => {
+      if (!context.currentUser) {
+        throw new AuthenticationError("not authenticated");
+      }
+
       const authorName = args.author;
       let author = await Author.findOne({ name: authorName });
       let book;
@@ -111,7 +120,11 @@ const resolvers = {
       }
       return book;
     },
-    editAuthor: async (root, args) => {
+    editAuthor: async (root, args, context) => {
+      if (!context.currentUser) {
+        throw new AuthenticationError("not authenticated");
+      }
+
       const author = await Author.findOne({ name: args.name });
       if (!author) {
         return null;
@@ -125,20 +138,34 @@ const resolvers = {
       const user = new User({ ...args });
       return user.save();
     },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username });
+
+      if (!user || args.password !== "secret") {
+        throw new UserInputError("wrong credentials");
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      };
+
+      return { value: jwt.sign(userForToken, JWT_SECRET) };
+    },
   },
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  // context: async ({ req }) => {
-  //   const auth = req ? req.headers.authorization : null;
-  //   if (auth && auth.toLowerCase().startsWith("bearer ")) {
-  //     const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
-  //     const currentUser = await User.findById(decodedToken.id);
-  //     return { currentUser };
-  //   }
-  // },
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null;
+    if (auth && auth.toLowerCase().startsWith("bearer ")) {
+      const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
+      const currentUser = await User.findById(decodedToken.id);
+      return { currentUser };
+    }
+  },
 });
 
 server.listen().then(({ url }) => {
