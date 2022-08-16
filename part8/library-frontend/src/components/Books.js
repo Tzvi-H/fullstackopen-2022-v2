103988528227279b9a1239116ec30269106ddad3
Tzvi-H <16guitar@gmail.com>
2022-08-15 18:23:30 -0600

    completed 8.19 and 8.20

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,8 +1,10 @@
 import { ALL_BOOKS } from "../queries";
 import { useQuery } from "@apollo/client";
+import { useState } from "react";
 
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS);
+  const [genre, setGenre] = useState("");
 
   if (!props.show) {
     return null;
@@ -11,13 +13,29 @@ const Books = (props) => {
   if (result.loading) {
     return <div>loading...</div>;
   }
-
-  const books = result.data.allBooks;
+  const books =
+    genre === "" || genre === "all genres"
+      ? result.data.allBooks
+      : result.data.allBooks.filter((b) => b.genres.includes(genre));
+  const genres = result.data.allBooks
+    .map((b) => b.genres)
+    .flat()
+    .reduce((acc, currentGenre) => {
+      if (!acc.includes(currentGenre)) {
+        acc.push(currentGenre);
+      }
+      return acc;
+    }, [])
+    .concat("all genres");
 
   return (
     <div>
       <h2>books</h2>
-
+      {genre && genre !== "all genres" && (
+        <p>
+          in genre <b>{genre}</b>
+        </p>
+      )}
       <table>
         <tbody>
           <tr>
@@ -34,6 +52,13 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      {genres.map((g) => {
+        return (
+          <button key={g} onClick={() => setGenre(g)}>
+            {g}
+          </button>
+        );
+      })}
     </div>
   );
 };
